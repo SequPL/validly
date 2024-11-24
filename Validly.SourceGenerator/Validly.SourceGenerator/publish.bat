173d3857c58042e735fcc030bb6596281a1ecb47
Roman Jámbor <hookcze@gmail.com>
2024-11-24 04:50:37 +0100

    chore: project configuration

--- Validly.SourceGenerator/Validly.SourceGenerator/publish.bat
+++ Validly.SourceGenerator/Validly.SourceGenerator/publish.bat
@@ -1,3 +1,3 @@
 dotnet pack -c Release
 set /p version=Package version:
-dotnet nuget push bin\Release\Validly.SourceGenerator.%version%.nupkg --api-key %NUGET_Validly_API_KEY% --source https://api.nuget.org/v3/index.json
+dotnet nuget push bin\Release\Validly.SourceGenerator.%version%.nupkg --api-key %NUGET_VALIDLY_API_KEY% --source https://api.nuget.org/v3/index.json
