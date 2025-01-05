267b578e0ef4b53805b0d611fc599b94f0e24267
Roman Jámbor <hookcze@gmail.com>
2025-01-06 00:01:41 +0100

    feat(Extensions.AspNetCore): refactored ValidationHttpResult

--- Validly.Extensions.AspNetCore/ValidationHttpResult.cs
+++ Validly.Extensions.AspNetCore/ValidationHttpResult.cs
@@ -23,41 +23,7 @@ public class ValidationHttpResult : IResult
 
 		try
 		{
-			await httpContext.Response.WriteAsync(
-				"""
-				{
-				  "type": "https://tools.ietf.org/html/rfc9110#section-15.5.1",
-				  "title": "One or more validation errors occurred.",
-				  "status": 400,
-				  "errors": [
-
-				"""
-			);
-
-			for (int propIndex = _validationResult.Properties.Count - 1; propIndex >= 0; propIndex--)
-			{
-				PropertyValidationResult prop = _validationResult.Properties[propIndex];
-
-				for (int errorIndex = prop.Messages.Count - 1; errorIndex >= 0; errorIndex--)
-				{
-					ValidationMessage error = prop.Messages[errorIndex];
-
-					// TODO: This allocates memory; should be optimized if possible
-					await httpContext.Response.WriteAsync(
-						$$"""
-						    {
-						      "detail": "{{error.Message}}",
-						      "resourceKey": "{{error.ResourceKey}}",
-						      "args": [{{error.ArgsJson}}],
-						      "pointer": "#{{prop.PropertyPath}}",
-						      "fieldName": "{{prop.PropertyDisplayName}}"
-						    }{{(propIndex == 0 && errorIndex == 0 ? string.Empty : ",")}}
-						"""
-					);
-				}
-			}
-
-			await httpContext.Response.WriteAsync("\r\n  ]\r\n}");
+			await httpContext.Response.WriteAsync(_validationResult.GetProblemDetailsJson());
 		}
 		finally
 		{
